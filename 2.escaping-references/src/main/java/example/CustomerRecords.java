bc4f48bedd4cd5181caa5a1e790081a6abc3bb51
SLem <slemjet@gmail.com>
2019-09-15 18:06:31 +0200

    Escaping references

--- 2.escaping-references/src/main/java/example/CustomerRecords.java
+++ 2.escaping-references/src/main/java/example/CustomerRecords.java
@@ -1,3 +1,5 @@
+package example;
+
 import java.util.HashMap;
 import java.util.Map;
 
---
+++ 2.escaping-references/src/main/java/exercise/BookReadOnly.java
@@ -0,0 +1,13 @@
+package exercise;
+
+public interface BookReadOnly {
+    int getId();
+
+    String getTitle();
+
+    String getAuthor();
+
+    String toString();
+
+    Price getPrice();
+}
